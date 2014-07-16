51a7f8818daa76181c986031b44e53c8ea92419e
wiseleo <github@crashproofsolutions.com>
2014-07-16 03:37:45 -0700

    Added a quick debug statement

--- restore_date_display.js
+++ restore_date_display.js
@@ -1,4 +1,5 @@
 (function () {
+	console.log('Bookmarklet LOADED...');
 	var linkNode = document.createElement('link');
 	linkNode.rel = 'stylesheet';
 	linkNode.href = 'https://raw.githubusercontent.com/wiseleo/Craigslist-Gallery-Bookmarklet/master/override-date-display-cl.css';
