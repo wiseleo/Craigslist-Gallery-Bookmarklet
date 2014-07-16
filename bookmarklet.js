8c8e8d35cc421e52e0b13a0ade56b830289d1860
wiseleo <github@crashproofsolutions.com>
2014-07-16 03:46:21 -0700

    Update and rename bookmarklet.html to bookmarklet.js
    Stripped HTML

---
+++ bookmarklet.js
@@ -0,0 +1,4 @@
+Select the line below and drag it to your Bookmarks bar.
+
+javascript:(function(){var fixdate = document.createElement('script');fixdate.src = 'https://raw.githubusercontent.com/wiseleo/Craigslist-Gallery-Bookmarklet/master/restore_date_display.js';document.body.appendChild(s);})();">Drag me to the bookmarks bar</a></h1>
+
