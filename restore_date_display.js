1d9f9504e85e2814187052a94691959d739dd737
wiseleo <github@crashproofsolutions.com>
2014-07-16 03:12:52 -0700

    Added the raw Github link for CSS.
    This should now work. :)

--- restore_date_display.js
+++ restore_date_display.js
@@ -1,1 +1,6 @@
-cl
+javascript: (function () {
+	var linkNode = document.createElement('link');
+	linkNode.rel = 'stylesheet';
+	linkNode.href = 'https://raw.githubusercontent.com/wiseleo/Craigslist-Gallery-Bookmarklet/master/override-date-display-cl.css';
+	document.getElementsByTagName('head')[0].appendChild(linkNode);
+})();
