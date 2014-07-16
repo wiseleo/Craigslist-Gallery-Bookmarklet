26784f821b7e6013b76ac8240e2bf7ed01c72daa
wiseleo <github@crashproofsolutions.com>
2014-07-16 03:54:06 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,4 +1,7 @@
-Select the line below and drag it to your Bookmarks bar.
+Select the entire block below and drag it to your Bookmarks bar.
 
-javascript:(function(){var fixdate = document.createElement('script');fixdate.src = 'https://raw.githubusercontent.com/wiseleo/Craigslist-Gallery-Bookmarklet/master/restore_date_display.js';document.body.appendChild(fixdate);})();"
-
+javascript:(function(){
+  var fixdate = document.createElement('script');
+  fixdate.src = 'https://raw.githubusercontent.com/wiseleo/Craigslist-Gallery-Bookmarklet/master/restore_date_display.js';
+  document.body.appendChild(fixdate);
+})();
